c9e832ef2668bab79d0f3390540ee39673fb4a52
Manou1993 <manolotsoadaniel@gmail.com>
2019-10-29 09:51:22 +0300

    2.3.3. Des liens

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'contact/show'
+  get "contact/show"
   # get "team/show", as: "privacy_policy"
   get "/team", to: "team#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
